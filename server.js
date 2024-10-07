8967322a05a52f7021acfe1f0683b0286a4d7fe5
mihir <mihir.pande22@pccoepune.org>
2024-10-07 19:46:20 +0530

    tried adding cursor, now converting code to react

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const server = http.createServer(app);
 
 // Setup WebSocket server
+const username = 'client 1'
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
@@ -14,10 +15,12 @@ wss.on('connection', (ws) => {
 
   // Listen for messages from clients
   ws.on('message', (message) => {
+    const data = JSON.parse(message);
+
     // Broadcast the message to all other connected clients
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(data));
       }
     });
   });
